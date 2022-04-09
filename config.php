56c30358211e9445910e92b060347accbcc855dd
majudev <maju@majudev.net>
2022-04-09 20:27:36 +0200

    Change version in default config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $config = array();
 
 $config["page_name"] = "Zwierz";
-$config["version"] = "0.1-beta";
+$config["version"] = "0.2-beta";
 $config["maindev"] = "majudev nobody@example.com";
 
 $config["base_url"] = "/zwierz";
